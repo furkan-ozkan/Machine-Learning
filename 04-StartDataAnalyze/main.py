df09db766549ed4f6023c725d0a0800f496ffc69
Furkan-Ozkan <54714384+furkan-ozkan@users.noreply.github.com>
2021-04-18 17:30:57 +0300

    save

--- 04-StartDataAnalyze/main.py
+++ 04-StartDataAnalyze/main.py
@@ -2,24 +2,63 @@
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import scipy as spcs
+import matplotlib.pyplot as plt
+
 #DataSet
-dataSet = sns.load_dataset("diamonds")
-db = dataSet.copy()
+dataSet = sns.load_dataset("planets")
+df = dataSet.copy()
 
 #Dataset Analyze
-print(db.head())
-print(db.tail())
+print(df.head())
+print(df.tail())
 
-print(db["carat"].count())
+print(df["method"].count())
 
-print(db.dtypes)
-db.cut = pd.Categorical(db.cut)
-print(db.dtypes)
+print(df.info())
 
-print(db.shape)
-print(db.columns)
+print(df.dtypes)
+df.method = pd.Categorical(df.method)
+print(df.dtypes)
 
-print(db.describe().T)
-print(db.describe(include="all").T)
+print(df.shape)
+print(df.columns)
+
+print(df.describe().T)
+print(df.describe(include="all").T)
+
+#Missing values
+print("Eksik Veri Var mı ? : ", df.isnull().values.any())
+
+print("Toplam Eksik Veri Sayıları : \n", df.isnull().sum())
+
+df["orbital_period"].fillna(0, inplace = True)
+df["mass"].fillna(df.mass.mean(), inplace = True)
+df["distance"].fillna(df.distance.mean(), inplace = True)
+
+print("Toplam Eksik Veri Sayıları : \n", df.isnull().sum())
+
+print(df.method.unique())
+
+print(df.method.value_counts())
+
+#matplotlib graph
+#df["method"].value_counts().plot.barh().set_title("Titles")
+#plt.show()
+#df["number"].value_counts().plot.barh()
+#plt.show()
 
 
+#Dia Dataset process
+dataSet= sns.load_dataset("diamonds")
+df=dataSet.copy()
+
+print(df.cut.head())
+
+cut_Category_Order = ["Fair" , "Good" , "Very Good" , "Premium" , "Ideal"]
+df.cut = df.cut.astype(pd.CategoricalDtype(categories=cut_Category_Order , ordered = True))
+
+print(df.cut.head(1))
+
+sns.barplot(x = df.cut , y = df.cut.index , data = df)
+plt.show()
